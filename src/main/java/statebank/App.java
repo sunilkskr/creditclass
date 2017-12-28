95f005e72f80f2abb9b0e978de163986992155bf
sunilkskr <sunggdfg@gmail.com>
2017-12-28 10:02:28 +0530

    first commit

---
+++ src/main/java/statebank/App.java
@@ -0,0 +1,13 @@
+package statebank;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
